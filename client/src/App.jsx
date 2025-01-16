38937b3dd09c6c6fca2defdf5d4ae78c5179f1ec
muhsina <muhsinamolty44@gmail.com>
2025-01-16 17:02:29 +0530

    created footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 import Registration from './components/user/Registration'
 import Login from './components/user/login'
-// import Navnar from './components/user/hero/Navnar'
 import Navnar from './components/user/hero/navnar'
+import { BrowserRouter,Router,Route } from 'react-router-dom'
+import ShopCatagory from './pages/ShopCatagory'
 
 
 function App() {
 
   return (
     <>
-      <h1>
+      
+       <BrowserRouter>
+       <Navnar />
+       <Router>
+             <Route path='/' element={<collections />}/>
+             <Route path='/' element={<ShopCatagory />}/>
+             <Route path='/' element={<ShopCatagory />}/>
+             <Route path='/' element={<ShopCatagory />}/>
+
+        </Router>
+       
+       </BrowserRouter>
+
         <Registration />
         <Login />
-        <Navnar />
-
-         </h1>
+      
       </>
     
   )
